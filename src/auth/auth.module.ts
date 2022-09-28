a648ce0fff03fb4e9da43635059bda9c441e8514
Rafal <rgrajcar@gmail.com>
2022-09-28 22:02:44 +0200

    added mail service to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
 import { RoleGuard } from './role.guard';
+import { MailService } from 'src/mail/mail.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, RoleGuard],
+  providers: [AuthService, JwtStrategy, RoleGuard, MailService],
   exports: [JwtStrategy],
 })
 export class AuthModule {}
