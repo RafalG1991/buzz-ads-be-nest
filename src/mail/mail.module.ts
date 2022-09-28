c6ca9b3be852c18b11d51a4d7385fd7ee762bec6
Rafal <rgrajcar@gmail.com>
2022-09-28 21:30:21 +0200

    fixed mail module by adding mailer config to import and mail service to export

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
+import mailerConfig = require('../config/mailer.config');
+import {MailerModule} from "@nestjs-modules/mailer";
 
 @Module({
-  providers: [MailService]
+  imports: [MailerModule.forRoot(mailerConfig)],
+  providers: [MailService],
+  exports: [MailService],
 })
 export class MailModule {}
