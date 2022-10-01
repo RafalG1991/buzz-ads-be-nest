8be892778477d558f494695f0392bad3bddda38c
Rafal <rgrajcar@gmail.com>
2022-10-01 17:46:18 +0200

    added create ad controller with AuthGuard

--- src/ad/ad.controller.ts
+++ src/ad/ad.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {Controller, Get, Post, Body, Patch, Param, Delete, UseGuards} from '@nestjs/common';
 import { AdService } from './ad.service';
 import { CreateAdDto } from './dto/create-ad.dto';
 import { UpdateAdDto } from './dto/update-ad.dto';
+import {UserObj} from "../decorators/user-obj.decorator";
+import {User} from "../user/entities/user.entity";
+import {AuthGuard} from "@nestjs/passport";
 
 @Controller('ad')
 export class AdController {
   constructor(private readonly adService: AdService) {}
 
-  @Post()
-  create(@Body() createAdDto: CreateAdDto) {
-    return this.adService.create(createAdDto);
+  @Post('/create')
+  @UseGuards(AuthGuard('jwt'))
+  create(@UserObj() user: User, @Body() newAd: CreateAdDto) {
+    return this.adService.createAd(user, newAd);
   }
 
   @Get()
